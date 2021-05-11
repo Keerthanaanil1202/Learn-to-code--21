d10b9b493b672bd50a2015bdab49dfbebad7514b
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-11 20:27:43 +0530

    Add files via upload

---
+++ coding 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() 
+{
+char a;
+int b;
+float g ;
+double d;
+    
+    scanf("%c",&a);
+    scanf("%d",&b);
+    scanf("%f",&g);
+    scanf("%lf",&d);
+    
+    printf("%c\n", a);
+    printf("%d\n", b);
+    printf("%f\n", g);
+    printf("%lf", d);
+
+    return 0;
+}
