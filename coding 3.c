4482f3b5342af68b14f78ab16d13ce2161fd3f90
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-12 19:43:22 +0530

    Add files via upload

---
+++ coding 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+  int main()
+{ 
+   int a, b;
+   int sum, sub, multi;
+   float div;
+
+   scanf("%d%d",&a,&b);
+
+   sum= a+b;
+   sub= a-b;
+   multi= a*b;
+   div= (float) a/b;
+ 
+   printf("%d \n",sum); 
+   printf("%d \n",sub);
+   printf("%d \n",multi); 
+   printf("%0.2f\n" ,div); 
+ 
+ 
+  return 0; 
+}
+    
---
+++ coding 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+  int main()
+{ 
+   double no, root;
+
+   scanf("%lf",&no);
+
+   root= sqrt(no);
+ 
+   printf("%0.1lf\n",root); 
+ 
+  return 0; 
+}
+    
