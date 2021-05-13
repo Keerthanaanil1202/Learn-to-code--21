7fd83f1592c1e8c77b4c91c8022a12e3638e7188
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-13 21:44:39 +0530

    Add files via upload

---
+++ coding 5(2) .c
@@ -0,0 +1,25 @@
+Write a C program to determine the areas of square, circle and triangle 
+taking length, radius,base and height repectively from the user input. 
+#include <stdio.h>
+#define PI 3.14
+
+  int main()
+{ 
+   float a,r,b,h,area2,area3;
+   int area1;
+
+   scanf("%f", &a);
+   scanf("%f", &r);
+   scanf("%f%f", &b, &h) ;
+
+   area1=a*a;
+   area2=PI*r*r;
+   area3=0.5*b*h;
+ 
+   printf("%d\n",area1); 
+   printf("%0.1f\n",area2);
+   printf("%0.2f",area3);
+ 
+  return 0; 
+}
+    
---
+++ coding 6(2) .c
@@ -0,0 +1,16 @@
+Write a C program to convert fahrenheit to celsius. 
+#include <stdio.h>
+
+  int main()
+{ 
+   float C, F;
+
+   scanf("%f", &F);
+
+   C=((F-32)*5)/9;
+
+   printf("%0.2f",C); 
+ 
+  return 0; 
+}
+    
