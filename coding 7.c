2561eb013860cbe2844f19eb101c9ad1eaacb07a
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-14 22:18:48 +0530

    Add files via upload

---
+++ coding 7.c
@@ -0,0 +1,22 @@
+Write a C program to verify if a given year is a leap year or not. 
+#include <stdio.h>
+
+  int main()
+{ 
+  int year;
+   
+   scanf("%d",&year);
+  
+   if((year%400==0)||((year%4==0)&&(year%100!=0)))
+    { 
+      printf("Yes");
+    }
+   else
+    { 
+      printf("No");
+    }
+    
+ return 0;
+
+}
+    
---
+++ coding 8.c
@@ -0,0 +1,60 @@
+Write a C program to find the maximum and minimum from the 3 user inputted numbers. 
+#include <stdio.h>
+
+  int main()
+{ 
+  int num1, num2, num3, min,max;
+  
+  scanf("%d%d%d",&num1,&num2,&num3);
+  if(num1>num2)
+   { 
+     if(num1>num3)
+      { 
+        max=num1;
+      }
+     else
+      { 
+        max=num3;
+      }
+    }
+  else 
+   { 
+     if(num2>num3) 
+       { 
+         max=num2;
+       }
+     else
+       {
+         max=num3;
+       }
+    }
+    
+  printf("%d\n",max);
+  
+    if(num1<num2) 
+     { 
+       if(num1<num3)
+        { 
+          min=num1;
+        }
+       else
+        { 
+          min=num3;
+        }
+      }
+    else
+      { 
+        if(num2<num3)
+          { 
+            min= num2;
+          }
+        else
+          { 
+            min=num3;
+          }
+       }
+  printf("%d", min);
+ return 0;
+
+}
+    
