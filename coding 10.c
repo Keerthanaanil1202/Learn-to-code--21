2a082d4c748c982dd6eabcee7213cb1305310554
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-15 21:41:23 +0530

    Add files via upload

---
+++ coding 10.c
@@ -0,0 +1,27 @@
+Write a C program to determine the factorial of a number. 
+#include <stdio.h>
+
+  int main()
+{ 
+   int n, i,ans=1;
+  
+   scanf("%d", &n);
+ 
+  if(n<0)
+   {
+     printf("ERROR");
+   }
+  else
+   {
+    for(i=1;i<=n;i++)
+    {
+     ans=ans*i;
+    }
+ 
+   printf("%d",ans);
+   }
+  
+ return 0;
+
+}
+    
---
+++ coding 9.c
@@ -0,0 +1,33 @@
+In a sequence of numbers from 1-100 replace the numbers divisible by 3 with 
+" FIZZ" replace the numbers divisible by 5 with "BUZZ" and replace numbers 
+divisible by 15 with "FIZZBUZZ" and print result.
+#include <stdio.h>
+
+  int main()
+{ 
+   int i;
+ 
+for(i=1;i<=100;i++)
+ { 
+   if(i%15==0)
+    { 
+      printf("FIZZBUZZ ");
+    }
+   else if(i%3==0)
+    { 
+      printf("FIZZ ");
+    }
+   else if(i%5==0)
+    {
+      printf("BUZZ ");
+    }
+   else
+    {
+      printf("%d ",i);
+    }
+   }
+
+ return 0;
+
+}
+    
