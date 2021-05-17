940bdc386e55922dee8d0b0821c4d9420514bb5f
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-17 21:29:29 +0530

    Add files via upload

---
+++ coding 11.c
@@ -0,0 +1,45 @@
+Write a C program to accept marks of 3 subjects. Calculate the percentage and respective grade
+according to following: percent>=90 Grade A, percent>=80 Grade B, percent>=70 Grade C, percent>=60 
+Grade D, percent>=40 Grade E and percent<40 Grade F. 
+#include <stdio.h>
+
+  int main()
+{
+  int a, b, c;
+  float percent;
+
+  scanf("%d%d%d",&a,&b,&c);
+  
+  percent=(a+b+c)/3.0;
+
+ printf("%0.2f\n",percent);
+
+  if(percent>=90)
+   {
+     printf("A");
+   }
+ else if(percent>=80)
+   { 
+     printf("B");
+   }
+ else if(percent>=70) 
+   {
+     printf("C");
+   }
+ else if(percent>=60) 
+   {
+     printf("D");
+   }
+ else if(percent>=40)
+   { 
+     printf("E");
+   }
+ else 
+   {
+     printf("F");
+   }
+  
+ return 0;
+
+}
+    
---
+++ coding 12.c
@@ -0,0 +1,36 @@
+Write a C program to determine a triangle is valid from the given sides. 
+#include<stdio.h>
+
+ int main() 
+ { 
+   int a,b,c;
+   
+   scanf("%d%d%d",&a, &b, &c);
+    
+    if((a+b)>c)
+      { 
+        if((b+c)>a)
+          { 
+            if((a+c)>b)
+              {
+                printf("Yes");
+              }
+             else
+              {
+                printf("No");
+              }
+           }
+         else
+           { 
+             printf("No");
+           }
+       }
+    else
+     {
+       printf("No");
+     }
+    
+ return 0;
+
+}
+    
