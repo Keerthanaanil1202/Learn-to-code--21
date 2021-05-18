bd18fe58987a5b2c0f071562540908841688d110
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-18 21:35:56 +0530

    Add files via upload

---
+++ coding 14.c
@@ -0,0 +1,43 @@
+Write a C program to display a calculator with a menu consisting of each operation to
+perform on the numbers using switch case statement. 
+#include<stdio.h>
+
+ int main() 
+ { 
+   float a,b;
+  int option;
+ printf(" 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n"); 
+  
+  scanf("%d\n", &option);
+  scanf("%f%f", &a, &b);
+  
+  switch(option) 
+  {
+    case 1:
+       printf("%0.0f\n",a+b);
+       break;
+    case 2:
+       printf("%0.0f\n",a-b);
+       break;
+    case 3:
+       printf("%0.0f\n",a*b);
+       break;
+    case 4:
+      if(b==0) 
+       {
+         printf("Cannot be determined");
+       }
+      else 
+      {
+       printf("%0.1f",a/b);
+      }
+       break;
+    default:
+       printf("Error") ;
+   }
+ 
+ return 0;
+
+}
+ 
+     
