74d5d4555d97becba33d96d9a3bedcc404662da7
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-19 21:56:34 +0530

    Add files via upload

---
+++ coding 15.c
@@ -0,0 +1,27 @@
+Write a C program to identify if a number is palindrome or not.
+#include<stdio.h>
+
+ int main() 
+ { 
+   int a,r=0,rem,p;
+ scanf("%d",&a);
+  p=a;
+ while(a!=0)
+  { 
+    rem=a%10;
+    r=r*10+rem;
+    a/=10;
+  }
+ if(p==r) 
+  {
+    printf("Yes");
+  }
+ else
+  {
+    printf("No");
+  }
+ 
+return 0;
+
+} 
+     
---
+++ coding 16.c
@@ -0,0 +1,30 @@
+Write a C program to compute the multiplication table of 1-5 using do while. 
+#include<stdio.h>
+
+ int main() 
+ { 
+   int i,n, multi;
+do
+{
+printf("\n1.Table of 1\n2.Table of 2\n3.Table of 3\n4.Table of 4\n5.Table of 5\n6.Exit\n");
+scanf("%d",&n);
+
+if(n<6) 
+{ 
+  for(i=1;i<=10;i++)
+   {
+     multi=n*i;
+     printf("%d ",multi) ;
+   }
+}
+else if(n>6)
+{
+ printf("Enter a valid option\n");
+}
+}
+while(n!=6);
+return 0;
+
+}
+ 
+     
