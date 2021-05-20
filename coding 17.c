8a78f7bba0e49bfb636411ba50432a1042b8ca77
Keerthanaanil1202 <83959376+Keerthanaanil1202@users.noreply.github.com>
2021-05-20 22:04:40 +0530

    Add files via upload

---
+++ coding 17.c
@@ -0,0 +1,24 @@
+C program to find the HCF of two numbers. 
+#include<stdio.h>
+
+ int main() 
+ { 
+   int a,b, x, y,t, HCF;
+scanf("%d%d",&x,&y) ;
+
+ a=x;
+ b=y;
+ 
+while(b!=0)
+ {
+   t=b;
+   b=a%b;
+   a=t;
+ }
+HCF=a;
+printf("%d",HCF);
+
+return 0;
+
+} 
+     
---
+++ coding 18.c
@@ -0,0 +1,31 @@
+Write a C program to print the following number pattern
+55555
+ 4444
+  333
+   22
+    1
+#include<stdio.h>
+
+ int main() 
+ { 
+   int i,j,r;
+
+ scanf("%d",&r);
+ for(i=r;i>=1;i--) 
+  { 
+    for(j=i;j<r;j++) 
+      {
+        printf(" ");
+       
+      }
+    for(j=1;j<=i;j++) 
+     { 
+       printf("%d",i);
+     }
+    printf("\n");
+  }
+
+
+return 0;
+} 
+     
